e5f07cababb417913b8919caf5a0837da9812905
EC2 Default User <ec2-user@ip-172-31-63-81.ec2.internal>
2022-06-03 02:53:03 +0000

    Segunda carga

---
+++ src/main/java/com/utpltec/app/App.java
@@ -0,0 +1,13 @@
+lpackage com.utpltec.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
